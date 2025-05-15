e88507e29aad8af7b31dcb1a9004815e1100623f
chiba <edyzrg1118@gmail.com>
2025-05-16 02:48:42 +0900

    userモデルと関連テーブルの作成

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  has_many :visits, dependent: :destroy
+  has_many :memos, dependent: :destroy
+  has_many :recordings, dependent: :destroy
+  has_many :documents, dependent: :destroy
+  has_many :question_selections, dependent: :destroy
+  has_many :notifications, dependent: :destroy
+
+  validates :name, :email, presence :true
+  validates :email, :uniqueness: true
+end
